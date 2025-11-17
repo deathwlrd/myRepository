a2a70a318a0e3eea75c832d20ca4f2d343a01285
deathwlrd <anerideleon@gmail.com>
2025-11-17 15:13:03 -0600

    Email and Messages implemented in ContactMe

---
+++ contact.js
@@ -0,0 +1,26 @@
+function validateForm()
+{
+    let uname = document.contactMe.name.value;
+    let email = document.contactMe.email.value; 
+    let num = document.contactMe.phone.value; 
+    let message = document.contactMe.msg.value; 
+
+    if(uname == "" || email == "" || num == "" || message == "")
+        alert("Please fill all the fields!"); 
+    else if (num.length != 10)
+        alert("Only provide a 10-digit nmber"); 
+    else if (!email.includes("gmail.com") && !email.includes("outlook.com") && !email.includes("utrgv.edu"))
+        alert("Email adress should be either Gmail, Outlook or UTRGV"); 
+    else if (message.length < 5) 
+        alert("Please enter more detailed message"); 
+    else{ 
+        alert("Loading mail client"); 
+
+        let subject = 'Message via Portfolio Website ${uname}';
+
+        let body = 'From: ${email}\nPhone: ${num}\nMessage: ${message}\n';
+
+        let mailtolink = 'mailto:antonio.nerideleon01@utrgv.edu? subject=${encodeURIComponent(subject)}&body=&{encodeURIComponent(body)}'; 
+        window.location.href = mailtolink; 
+    }
+}
